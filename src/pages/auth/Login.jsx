3c8895d489a3de44d1eca6ec1955671ad378c000
Srivarshini-iHUB <srivarshini.r.ihub@snsgroups.com>
2025-10-09 17:36:26 +0530

    consol

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -37,7 +37,7 @@ export default function LoginForm() {
   const setUser = useAuthStore((state) => state.setUser);
 
 console.log("Login Schema:", loginSchema);
-console.log("Render LoginForm");
+console.log("Render LoginForm Component");
 
   const form = useForm({
     resolver: zodResolver(loginSchema),
